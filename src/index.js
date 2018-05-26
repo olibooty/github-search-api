a1865c3fb5ab588535f63d9ac382d890eac0b67b
Oli Booty <oliboots@gmail.com>
2018-05-26 18:57:13 +0100

    got search working

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import moment from "moment";
 // Use the moment library to get an easily formatted date
 const oneMonthAgo = moment().subtract(1, 'month').format("YYYY-MM-DD");
 console.log(oneMonthAgo);
+const oneMonthFormat = moment().subtract(1, 'month').format('Do MMMM YYYY');
+console.log(oneMonthFormat)
 
 // get the search query ready, with the inputted date
 // and empty string
-const url = `https://api.github.com/search/repositories?q=stars+created:>${oneMonthAgo}`;
-const lang = "+language:";
 let query = "";
+
+// const lang = "+language:";
+
 
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 
+
+input.textContent = "";
 input.focus();
 
 const request = new XMLHttpRequest();
@@ -21,34 +26,45 @@ form.onsubmit = () => {
   query = input.value;
   console.log(query);
 
+  // let url = `https://api.github.com/search/repositories?q=${query}+language:${query}+sort=stars+created:>${oneMonthAgo}`;
+  let url = `https://api.github.com/search/repositories?q=${query}+created:>${oneMonthAgo}&sort=stars`;
+
   // check if input contains empty string
   // if it does, omit language query
   if (query === "") {
-    console.log(url + query);
-    request.open('GET', url, true);
+    // console.log(url + query);
+    // request.open('GET', url, true);
+    alert("Please enter a valid language");
   }
   else {
     const fullQuery = `${url}+language:${query}`;
-    console.log(fullQuery);
-    request.open('GET', fullQuery, true);
-  }
+    console.log(url);
+    request.open('GET', url, true);
 
-  request.send();
-}
+    const heading = document.getElementById("heading");
+    const para = document.querySelector("#listing p");
+
+    heading.textContent = `Most Stars: '${query}'`;
+    para.textContent = `Repos created since ${oneMonthFormat}`;
+
+    request.send();
+  }
+};
 
 request.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     // Parse JSON
-    const json = JSON.parse(this.responseText)
+    const json = JSON.parse(this.responseText);
     const myArr = json.items;
     console.log(myArr);
 
     for (let i = 0; i <= 2; i++) {
       console.log(myArr[i]);
-      console.log(myArr[i].full_name)
+      console.log(myArr[i].full_name);
+      console.log(myArr[i].created_at);
     }
   }
-}
+};
 
 
 
