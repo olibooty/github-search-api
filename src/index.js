63ce715fa048a84ee00b024e32d61d8c8cd0a1ee
Oli Booty <oliboots@gmail.com>
2018-05-27 12:33:34 +0100

    did something useful with this fork

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import moment from "moment";
-import axios from 'axios';
 
 // Use the moment library to get an easily formatted date
 const oneMonthAgo = moment().subtract(1, 'month').format("YYYY-MM-DD");
@@ -24,17 +23,8 @@ form.onsubmit = () => {
 
   const url = `https://api.github.com/search/repositories?q=language:${query}+created:>${oneMonthAgo}&sort=stars`;
 
-  // axios.get(url)
-  // .then(function (response) {
-  //   console.log(response.request.response);
-  // })
-  // .catch(function (error) {
-  //   console.log(error);
-  // });
-
   // check if input contains empty string
   // if it does, throw alert
-
   if (query === "") {
     alert("Please enter a valid language");
   }
@@ -56,14 +46,13 @@ form.onsubmit = () => {
 
 request.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-  // if (this.readyState == 4) {
     // Parse JSON
     const json = JSON.parse(this.responseText);
     const myArr = json.items;
     console.log(myArr);
 
     for (let i = 0; i <= 2; i++) {
-
+      // Created object to hold repo values
       const htmlVal = {
         aHref: myArr[i].html_url,
         aVal: myArr[i].full_name,
@@ -75,6 +64,45 @@ request.onreadystatechange = function() {
       for (let val in htmlVal) {
         console.log(htmlVal[val])
       }
+
+      function createPTag(text, parent) {
+        const ele = document.createElement("p");
+        ele.textContent = text;
+        parent.appendChild(ele);
+      }
+
+      const parentDiv = document.createElement("div");
+      parentDiv.className = "tile";
+
+      const aTag = document.createElement("a");
+      aTag.href = htmlVal.ahref;
+      aTag.textContent = htmlVal.aVal;
+      parentDiv.appendChild(aTag);
+
+      // const desc = document.createElement("p");
+      // desc.textContent = htmlVal.pDesc;
+      // parentDiv.appendChild(desc);
+
+      createPTag(htmlVal.pDesc, parentDiv);
+
+      const childDiv = document.createElement("div");
+      childDiv.className = "info";
+
+      createPTag(htmlVal.pDate, childDiv);
+      createPTag(htmlVal.pStars, childDiv);
+
+      // const pOne = document.createElement("p");
+      // pOne.textContent = htmlVal.pDate;
+      // childDiv.appendChild
+
+      // const pTwo = document.createElement("p");
+      // pTwo.textContent = htmlVal.pStars;
+
+
+      parentDiv.appendChild(childDiv);
+
+      const repos = document.getElementById("repos");
+      repos.appendChild(parentDiv);
     }
   }
 };
